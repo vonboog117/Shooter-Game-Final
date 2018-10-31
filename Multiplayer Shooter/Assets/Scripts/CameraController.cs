94b19a00eef1a9274fefd9e165e88ec514a86718
vonboog117 <vonboog118@gmail.com>
2018-10-31 11:24:00 -0500

    Created working player prefab

---
+++ Multiplayer Shooter/Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float verticalSensitivity;
+    public float horizontalSensitivity;
+
+    public float minVertRotation = -45.0f;
+    public float maxVertRotation = 45.0f;
+
+    public float xRotation = 0;
+
+    private GameObject player;
+    public void SetPlayer(GameObject p){
+        player = p;
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Move(){
+        if (player != null){
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 2f, player.transform.position.z);
+        }
+    }
+
+    void Rotate(){
+        xRotation -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+        xRotation = Mathf.Clamp(xRotation, minVertRotation, maxVertRotation);
+
+        float delta = Input.GetAxis("Mouse X") * horizontalSensitivity;
+        float rotationY = transform.localEulerAngles.y + delta;
+
+        transform.localEulerAngles = new Vector3(xRotation, rotationY, 0);
+    }
+}
---
+++ Multiplayer Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float rotationSpeed;
+
+    private CharacterController characterController;
+    private Camera camera;
+
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+        camera = FindObjectOfType<Camera>();
+        camera.GetComponent<CameraController>().SetPlayer(this.gameObject);
+	}
+
+    void Update(){
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        Vector3 movement = ((camera.gameObject.transform.forward * z) + (camera.gameObject.transform.right * x) + new Vector3(0, -9.8f * Time.deltaTime, 0));
+
+        characterController.Move(movement);
+    }
+}
