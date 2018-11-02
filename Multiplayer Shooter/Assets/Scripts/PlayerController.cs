1fb5620fe51a8701d28d474a05224ac9dbbccaf5
vonboog117 <vonboog118@gmail.com>
2018-11-02 17:39:21 -0500

    Added ladder to the building (not finished)

--- Multiplayer Shooter/Assets/Scripts/PlayerController.cs
+++ Multiplayer Shooter/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour {
     private CharacterController characterController;
     private Camera camera;
 
+    private bool isOnLadder;
+
 	void Start () {
         characterController = GetComponent<CharacterController>();
         camera = FindObjectOfType<Camera>();
@@ -17,11 +19,31 @@ public class PlayerController : MonoBehaviour {
 	}
 
     void Update(){
-        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        if (!isOnLadder) { 
+            float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+            float z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
-        Vector3 movement = ((camera.gameObject.transform.forward * z) + (camera.gameObject.transform.right * x) + new Vector3(0, -9.8f * Time.deltaTime, 0));
+            Vector3 movement = ((camera.gameObject.transform.forward * z) + (camera.gameObject.transform.right * x) + new Vector3(0, -9.8f * Time.deltaTime, 0));
 
-        characterController.Move(movement);
+            characterController.Move(movement);
+        }else{
+            float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+            Vector3 movement;
+
+            if (y != 0){
+                movement = (this.gameObject.transform.up * y) + (camera.gameObject.transform.right * x);
+            }else if (Input.GetKey(KeyCode.LeftShift)){
+                movement = camera.gameObject.transform.right * x;
+            }
+        }
     }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.tag == "Ladder"){
+            isOnLadder = true;
+        }
+    }
+    
 }
