8e51b69b9c44cc69a1c2c03ce9788c9e961a5ae8
vonboog117 <vonboog118@gmail.com>
2019-04-05 11:42:49 -0500

    Created weapon spawn area and began Hazard script, need to make a way for players to leave the lava

---
+++ Multiplayer Shooter/Assets/Scripts/Hazard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+
+    public int damage;
+    public float damageRepeat;
+    public bool canKill;
+
+    private void OnTriggerStay(Collider other){
+        Debug.Log("Ahhhhh Lava!");
+    }
+}
